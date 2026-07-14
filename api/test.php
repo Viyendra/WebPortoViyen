07e5fbd5cd238a960fc62f89111827522de74051
Viyendra <muhammadviyendra@gmail.com>
2026-07-14 14:01:54 +0700

    fix: remove conflicting legacy PHP api files

--- api/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Bare minimum test - no includes, no dependencies
-header('Content-Type: application/json');
-echo json_encode([
-    'status' => 'PHP works!',
-    'php_version' => phpversion(),
-    'time' => date('Y-m-d H:i:s'),
-]);
